87785e3f833644127ea3e9a71a5b0e15edf3a28e
Jason Mac <jasonvanmac@gmail.com>
2024-09-22 16:45:11 -0700

    Term Project

---
+++ push.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Pushing script
+"""
+
+import subprocess
+
+def run():
+    """
+    run function
+    """
+    try:
+        subprocess.run(["git", "add", "."], check=True)
+        subprocess.run(["git", "commit", "-m", "Term Project"], check=True)
+        subprocess.run(["git", "push"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    run()
