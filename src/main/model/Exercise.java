a8d19c9ebdd03e41a27fbe54db3b87c8add7b323
Jason Mac <jasonvanmac@gmail.com>
2024-10-01 16:13:57 -0700

    Added implmentation for Exercise

--- src/main/model/Exercise.java
+++ src/main/model/Exercise.java
@@ -1,31 +1,42 @@
 package model;
 
+import model.Set.Unit;
+import java.util.ArrayList;
+
 // Stores information of the amount of sets done in a given exercise
 public class Exercise {
+
+    private String name;
+    private ArrayList<Set> sets; 
 
     // REQRUIRES: non-empty name
     // EFFECTS: creaates an exercise with given name and no sets initially done
     public Exercise(String name) {
-        // stub
+        this.name = name;
+        this.sets = new ArrayList<>();
     }
 
     // MODIFIES: this
     // EFFECT: adds a set to the list of sets for this exercise
     public void  addSet(Set set) {
-        // stub 
+        sets.add(set);
     }
 
-    // EFFECT: returns the accumulative setVolumes done in this exercise 
-    public double getExerciseVolume() {
-        return 0.0; // stub
+    // EFFECT: returns the accumulative setVolumes done in this exercise for specifed unit 
+    public double getVolume(Unit u) {
+        double volume = 0.0;
+        for (Set set : sets) {
+            volume += set.getSetVolume(u);
+        }
+        return volume;
     }
 
     // EFFECT: Returns the amount of sets were perfoemd with this exercise
     public int getSetCount() {
-        return -1; // stub
+        return sets.size();
     }
 
-    public String getExerciseName() {
-        return new String(); // stub
+    public String getName() {
+        return name;
     }
 }
