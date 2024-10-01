c81b750cf23d6a11b0e336dbe45d5ddd235aa667
Jason Mac <jasonvanmac@gmail.com>
2024-10-01 15:17:04 -0700

    Implemented Set Class

--- src/main/model/Set.java
+++ src/main/model/Set.java
@@ -1,55 +1,85 @@
 package model;
 
+import java.lang.Math;
+
 // Class that models a a single set of an exercise in a workout
 public class Set {
 
     // Conversion factors
-    private static final double POUND_TO_KILOGRAMS = 0.45359;
-    private static final double KILOGRAM_TO_SPOUND = 2.2046;
+    private static final double POUNDS_TO_KILOGRAMS = 0.45359;
+    private static final double KILOGRAM_TO_POUNDS = 2.2046;
 
     // public enum for specifying between kilograms and pounds
     public enum Unit {
         KILOGRAMS,
         POUNDS,
     }
+
+    // Private Fields
+    private double weight;
+    private int repCount;
+    private Unit unit;
     
-    // REQUIRES: weight > 0 and weight must be at most one decimal place
+    // REQUIRES: weight > 0 and weight must be at most one decimal place and repCount > 0
     // EFFECT: Creates a set with the current weight, repCount, and unit specified
     public Set(double weight, int repCount, Unit unit) {
-        // stub
+        this.weight = weight;
+        this.repCount = repCount;
+        this.unit = unit;
     }
 
     // REQUIRES: weight > 0 and weight must be at most one decimal place
     // MODIFIES: this
     // EFFECT: changes the weight used in the set, with a unit specified
-    public void setWeight (double weight, Unit unit) {
-        // stub
+    public void setWeight(double weight, Unit unit) {
+        this.weight = weight;
+        this.unit = unit;
     }
 
     // REQUIRES: repCount > 0
     // MODIFIES: this
     // EFFECT: changes the rep count in the set with repCount
     public void setRepCount(int repCount) {
-        // stub
+        this.repCount = repCount;
     }
 
     // EFFECT: returns the set "volume", in other words returns weight * rep count with specified unit
-    public void getSetVolume(Unit unit) {
-        // stub
+    public double getSetVolume(Unit unit) {
+        double volume;
+        if(unit == Unit.KILOGRAMS) {
+            volume = this.getWeightInKilograms() * this.repCount;
+        } else {
+            volume = this.getWeightInPounds() * this.repCount;
+        }
+        return oneDecimal(volume);
     }
 
 
     // EFFECT: returns the weight used in the set in kilograms
     public double getWeightInKilograms() {
-        return 0.0; // stub
+        if(this.unit == Unit.KILOGRAMS) {
+            return this.weight;
+        }
+
+        double weightInKilograms = this.weight * POUNDS_TO_KILOGRAMS;
+        return oneDecimal(weightInKilograms); // stub
     }
 
     // EFFECT: returns the weight used in the set in pounds 
     public double getWeightInPounds() {
-        return 0.0; // stub
+        if(this.unit == Unit.POUNDS) {
+            return this.weight;
+        }
+
+        double weightInPounds = this.weight * KILOGRAM_TO_POUNDS;
+        return oneDecimal(weightInPounds); 
     }
 
     public int getRepCount() {
-        return -1; // stub
+        return this.repCount;
+    }
+
+    private double oneDecimal(double value) {
+        return Math.round(value * 10) / 10.0;
     }
 }
