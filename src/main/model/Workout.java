1fedb9bddabde0a801d0c9c69e5b43bcb921386c
Jason Mac <jasonvanmac@gmail.com>
2024-10-01 20:05:14 -0700

    Implemented Workout Class

--- src/main/model/Workout.java
+++ src/main/model/Workout.java
@@ -2,63 +2,96 @@ package model;
 
 import model.Set.Unit;
 
+
+import java.util.ArrayList;
+
+// Class that models a workout, which stores a list of exercises 
 public class Workout {
+    ArrayList<Exercise> exercises;
     
     // EFFECTS: Construct a workout with no exercises
     public Workout() {
-        // stub
+        this.exercises = new ArrayList<>();
     }
 
+    // REQUIRES: that given exercise with its given name is not alrady in the list
     // MODIFIES: this
     // EFFECTS: adds the exercise to list of exercises in the workout
     public void addExercise(Exercise exercise) {
-        //stub
+        exercises.add(exercise);
     }
 
     // MODIFIES: this
     // EFFECTS: removes the given exercise from list of exercises in workout at index and returns true
     //          if the index is out of bounds or list is empty, do nothing and return false
     public boolean removeExercise(int index) {
-        return true; // stub
+        if(outOfBounds(index) || exercises.isEmpty()) {
+            return false;
+        }
+        exercises.remove(index);
+        return true;
     }
 
     // MODIFIES: this
     // EFFECTS: replaces the exercie in the list at the specified index with given exercise, and return true
     //          if the index is out of bounds or list is empty, do nothing and return false
     public boolean replaceExercise(int index, Exercise exercise) {
-        return true; // stub
+        if(outOfBounds(index) || exercises.isEmpty()) {
+            return false;
+        }
+        exercises.set(index, exercise);
+        return true;
     }
 
     // EFFECTS: returns the given exercise at index from list of exercises in workout
-    //          returns null if index is out of bounds
+    //          returns null if index is out of bounds or list is empty
     public Exercise getExerciseByIndex(int index) {
-        return new Exercise(null); // stub
+        if(outOfBounds(index) || exercises.isEmpty()) {
+            return null;
+        }
+        return exercises.get(index);
     }
 
     // EFFECTS: returns the given exercise at index from list of exercises in workout
     //          returns null if exercise does not exist with the given name
     public Exercise getExerciseByName(String name) {
-        return new Exercise(null); // stub
+        Exercise toReturn = null;
+        for (Exercise exercise : exercises) {
+            if (exercise.getName().equals(name)) {
+                toReturn = exercise;
+                break;
+            }
+        }
+        return toReturn;
     }
 
     // MODIFIES: this
     // EFFECTS: clears all exercises from this list of exercises
     public void clearExercises() {
-        // stub
+        this.exercises.clear();
     }
 
     // EFFECTS: Return total workout volume in specified unit
     public double getVolume(Unit unit) {
-        return 0.0; // stub
+        double toReturn = 0;
+        for (Exercise exercise : exercises) {
+            toReturn += exercise.getVolume(unit);
+        }
+        return toReturn;
     }
 
     // EFFECTS: returns true if there are no exercises in this workout
     public boolean isEmpty() {
-        return true; // stub
+        return this.exercises.size() == 0;
     }
 
     // EFFECTS: return number of exercises in the workout
     public int getNumberOfExercises() {
-        return -1; // stub
+        return exercises.size();
+    }
+
+    // EFFECTS: returns if the index is out of bounds of exercises array
+    private boolean outOfBounds(int index) {
+        return (index < 0 || index >= exercises.size());
     }
 }
