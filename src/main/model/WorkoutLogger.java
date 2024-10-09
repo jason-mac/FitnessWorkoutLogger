244b296414bd31c2da03525e74d49cee633d2065
Jason Mac <jasonvanmac@gmail.com>
2024-10-09 14:25:29 -0700

    Added private specifiers, change documentation

--- src/main/model/WorkoutLogger.java
+++ src/main/model/WorkoutLogger.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 // Class that is able to store, and retrieve workouts given a date. 
 public class WorkoutLogger {
-    HashMap<String, Workout> workoutLogs;
+    private HashMap<String, Workout> workoutLogs;
 
     // EFFECTS: Creates a workout logger with no workouts logged
     public WorkoutLogger () {
@@ -28,18 +28,15 @@ public class WorkoutLogger {
         this.workoutLogs.remove(date);
     }
 
-
     // MODIFIES: this
     // EFFECTS: clears the entire workout logs
     public void clearLogs() {
         this.workoutLogs.clear();
     }
 
-
-
     // REQUIRES: workoutLogs.size() != 0
     // EFFECTS: returns a list of dates to which a workout has been stored
-    Set<String> getDates() {
+    public Set<String> getDates() {
         return this.workoutLogs.keySet();
     }
 
