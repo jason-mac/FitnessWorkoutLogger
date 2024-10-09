acd04230d5d624d4ea45035d0e2e6c47c4aef05b
Jason Mac <jasonvanmac@gmail.com>
2024-10-09 12:56:51 -0700

    Added further tests for improved functionlality

--- src/test/model/TestWorkoutLogger.java
+++ src/test/model/TestWorkoutLogger.java
@@ -1,9 +1,12 @@
 package model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
 
 public class TestWorkoutLogger {
     private WorkoutLogger logger;
@@ -38,5 +41,58 @@ public class TestWorkoutLogger {
         assertEquals(2, logger.getNumWorkoutsLogged());
         assertEquals(work, logger.getWorkout(date));
         assertEquals(workTwo, logger.getWorkout(dateTwo));
+    }
+
+    @Test
+    void testRemoveWorkout() {
+        String date = "12/12/2012";
+        this.logger.addWorkout(date, work);
+        assertEquals(1, this.logger.getNumWorkoutsLogged());
+        this.logger.removeWorkout(date);
+        assertEquals(0, this.logger.getNumWorkoutsLogged());
+    }
+
+    @Test
+    void testRemoveTwoWorkouts() {
+        String date = "12/12/2012";
+        String dateTwo = "01/01/2001";
+        Workout workTwo = new Workout("random");
+
+        this.logger.addWorkout(date, work);
+        this.logger.addWorkout(dateTwo, workTwo);
+
+        this.logger.removeWorkout(date);
+        assertEquals(1, this.logger.getNumWorkoutsLogged());
+
+        this.logger.removeWorkout(date);
+        assertEquals(0, this.logger.getNumWorkoutsLogged());
+    }
+
+    @Test
+    void testClearLogs() {
+        String date = "12/12/2012";
+        String dateTwo = "01/01/2001";
+        Workout workTwo = new Workout("random");
+
+        this.logger.addWorkout(date, work);
+        this.logger.addWorkout(dateTwo, workTwo);
+
+        this.logger.clearLogs();
+        assertEquals(0, logger.getNumWorkoutsLogged());
+    }
+
+    @Test 
+    void testGetDates() {
+        String date = "12/12/2012";
+        String dateTwo = "01/01/2001";
+        Workout workTwo = new Workout("random");
+
+        this.logger.addWorkout(date, work);
+        this.logger.addWorkout(dateTwo, workTwo);
+
+        Set<String> dates = this.logger.getDates();
+        assertTrue(dates.contains(date));
+        assertTrue(dates.contains(dateTwo));
+
     }
 }
