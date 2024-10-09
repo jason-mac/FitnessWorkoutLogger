9c2b781b399d6f2360ea5e237c7ee4fd38f7e24f
Jason Mac <jasonvanmac@gmail.com>
2024-10-09 12:40:34 -0700

    Implemented WorkoutLogger class and improved documentation

--- src/main/model/WorkoutLogger.java
+++ src/main/model/WorkoutLogger.java
@@ -2,26 +2,29 @@ package model;
 
 import java.util.HashMap;
 
+// Class that is able to store, and retrieve workouts given a date. 
 public class WorkoutLogger {
     HashMap<String, Workout> workoutLogs;
 
+    // EFFECTS: Creates a workout logger with no workouts logged
     public WorkoutLogger () {
         this.workoutLogs = new HashMap<>();
     }
 
+    // REQUIRES: date is int the format dd/mm/year
     // MODIFIES: this
     // EFFECTS: adds a workout at given date into the collection
     public void addWorkout(String date, Workout workout) {
-        //stub
+        this.workoutLogs.put(date, workout);
     }
 
     // EFFECTS: retrieves the workout at given date,
     //          otherwise returns nulls
     public Workout getWorkout(String date) {
-        return null; // stub
+        return this.workoutLogs.get(date);
     }
 
     public int getNumWorkoutsLogged() {
-        return 0; // stub
+        return this.workoutLogs.size();
     }
 }
