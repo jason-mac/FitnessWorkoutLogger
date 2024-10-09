142f31dd26a561d3e882fcac523f233caa898d39
Jason Mac <jasonvanmac@gmail.com>
2024-10-09 14:24:21 -0700

    Create main function and class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,4 +1,8 @@
 package ui;
 
 public class Main {
+    public static void main(String[] args) {
+        WorkoutLoggerApp workoutLoggerApp = new WorkoutLoggerApp();
+        workoutLoggerApp.run();
+    }
 }
