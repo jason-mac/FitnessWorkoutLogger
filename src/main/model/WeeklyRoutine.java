04204d9536c5bb30cf70e4fdb775ae9f5af2afcf
Jason Mac <jasonvanmac@gmail.com>
2024-10-20 14:56:28 -0700

    added equals method for weeklyroutine

--- src/main/model/WeeklyRoutine.java
+++ src/main/model/WeeklyRoutine.java
@@ -70,4 +70,32 @@ public class WeeklyRoutine {
     public void clearRoutine() {
         routine.clear();
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + routine.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        WeeklyRoutine other = (WeeklyRoutine) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (!routine.equals(other.routine))
+            return false;
+        return true;
+    }
 }
