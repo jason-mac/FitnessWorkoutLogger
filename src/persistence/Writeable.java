935c91aa93a00c07cc91e9e2a8cad6adbacd8c55
Jason Mac <jasonvanmac@gmail.com>
2024-10-20 13:10:36 -0700

    added writable class to persistence package

---
+++ src/persistence/Writeable.java
@@ -0,0 +1,16 @@
+package persistence;
+
+import org.json.JSONObject;
+
+import org.json.JSONObject;
+
+/*
+ * This code is based on code provided by JsonSerializationDemo 
+ * as part of the course CPSC 210 at University of British Coumbia
+ * Source: https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+ */
+
+public interface Writeable {
+    // EFFECTS: returns this as JSON object
+    JSONObject toJson();
+}
