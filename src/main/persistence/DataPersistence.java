00d43193ad863113f691d734083c0c83fcf800ac
Jason Mac <jasonvanmac@gmail.com>
2024-11-21 12:35:09 -0800

    Implemented DataPersistence class

--- src/main/persistence/DataPersistence.java
+++ src/main/persistence/DataPersistence.java
@@ -7,23 +7,27 @@ import model.WorkoutLogger;
 
 // Data persistence class that will deal with both saving and loading workoutLogger
 public class DataPersistence {
+    private JsonReader readerWorkoutLogs;
+    private JsonWriter writerWorkoutLogs;
 
     // EFFECTS: Instantiates JsonWriter and JsonReader for workout logger and sets the filePath 
     //          for which the Json file will be saved
     public DataPersistence(String filePath) {
-        //stub
+        this.writerWorkoutLogs = new JsonWriter(filePath);
+        this.readerWorkoutLogs = new JsonReader(filePath);
     }
 
     // EFFECTS: Given a workout logger, saves it to the appropiate file 
     //          throws FileNotFoundException if cannot be saved
     public void save(WorkoutLogger workoutLogs) throws FileNotFoundException {
-        //stub
+        writerWorkoutLogs.open();
+        writerWorkoutLogs.write(workoutLogs);
+        writerWorkoutLogs.close();
     }
 
     // EFFECTS: Retrives the workoutLogger from the Json file given the filePath
     //          and returns the workoutLogger
     public WorkoutLogger load() throws IOException {
-        return null;
-        //stub
+        return readerWorkoutLogs.readWorkoutLogs();
     }
 }
