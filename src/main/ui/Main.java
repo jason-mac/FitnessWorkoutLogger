6586511034a4832c7691583158fa162cb1331919
Jason Mac <jasonvanmac@gmail.com>
2024-11-21 12:27:14 -0800

    Change file structure for cli and gui versions

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,14 +1,31 @@
 package ui;
 
-import java.io.FileNotFoundException;
+import javax.swing.UIManager;
 
+// Entry point for the main application 
 public class Main {
     public static void main(String[] args) {
+
         FitnessLoggerApp workoutLoggerApp = new FitnessLoggerApp();
+
         try {
-            workoutLoggerApp.run();
-        } catch (FileNotFoundException e) {
-            System.out.println("Unable to run application: file not found");
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        workoutLoggerApp.runGui();
+
+        // try {
+        //     workoutLoggerApp.runCli();
+        // } catch (FileNotFoundException e) {
+        //     System.out.println("Unable to run application: file not found");
+        // }
     }
 }
+
---
+++ src/main/ui/gui/FitnessLoggerAppGui.java
@@ -0,0 +1,23 @@
+package ui.gui;
+
+import javax.swing.*;
+
+/*
+ * SOURCE CREDITS
+ * The code in this gui application takes inspiration from many sources:
+ * 
+ * 1) Bro Code on YouTube
+ * Link: https://www.youtube.com/watch?v=Kmgo00avvEw&t=4660s&pp=ygURYnJvIGNvZGUgamF2YSBndWk%3D
+ * 
+ * 2) The demo files from "Oracle Java Documentation"  
+ * Link: https://docs.oracle.com/javase/tutorial/uiswing/examples/components/index.html
+ * 
+ */
+public class FitnessLoggerAppGui extends JFrame {
+
+    // EFFECTS: Creates an instance of the FitenssLoggerAppGui
+    //          and instantiates appropiate fields for GUI and runs the JFrame
+    public FitnessLoggerAppGui() {
+        // stub
+    }
+}
